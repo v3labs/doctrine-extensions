1b11e0abd04265ad0e27e64861576ba167f1c0ab
Vladislav Veselinvo <vladislav@v3labs.com>
2013-01-04 20:32:45 +0200

    Initial commit

---
+++ src/V3labs/DoctrineExtensions/ORM/Timestampable/Timestampable.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace V3labs\DoctrineExtensions\ORM\Timestampable;
+
+use Datetime;
+
+trait Timestampable {
+
+    protected $createdAt;
+
+    protected $updatedAt;
+
+    /**
+     * @return DateTime
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @return DateTime
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+
+    public function setCreatedAt(DateTime $createdAt)
+    {
+        $this->createdAt = $createdAt;
+    }
+
+    public function setUpdatedAt(DateTime $updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+    }
+
+    protected function updateTimestampableFields()
+    {
+        if (is_null($this->createdAt)) {
+            $this->createdAt = new DateTime('now');
+        }
+
+        $this->updatedAt = new DateTime('now');
+    }
+}
---
+++ src/V3labs/DoctrineExtensions/ORM/Timestampable/TimestampableListener.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace V3labs\DoctrineExtensions\ORM\Timestampable;
+
+use Doctrine\ORM\Event\LoadClassMetadataEventArgs,
+    Doctrine\Common\EventSubscriber,
+    Doctrine\ORM\Events;
+
+
+class TimestampableListener implements EventSubscriber
+{
+    public function loadClassMetadata(LoadClassMetadataEventArgs $eventArgs)
+    {
+        $classMetadata = $eventArgs->getClassMetadata();
+
+        if (is_null($classMetadata->reflClass)) {
+            return;
+        }
+
+        if (in_array('V3labs\DoctrineBehaviours\Timestampable\Timestampable', $classMetadata->reflClass->getTraitNames())) {
+            /* Map fields */
+            $classMetadata->mapField(['fieldName' => 'createdAt', 'type' => 'datetime', 'nullable' => true]);
+            $classMetadata->mapField(['fieldName' => 'updatedAt', 'type' => 'datetime', 'nullable' => true]);
+
+            /* Add lifecycle callbacks */
+            $classMetadata->addLifecycleCallback('updateTimestampableFields', Events::prePersist);
+            $classMetadata->addLifecycleCallback('updateTimestampableFields', Events::preUpdate);
+        }
+    }
+
+    public function getSubscribedEvents()
+    {
+        return [Events::loadClassMetadata];
+    }
+}
